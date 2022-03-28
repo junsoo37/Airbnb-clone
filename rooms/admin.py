c3d55d58f5a38551d01f66b3aaad16b26afe2593
junsoo37 <junsoor37@gmail.com>
2022-03-28 10:03:04 +0900

    Implement: Set options to room admin panel 7.1

--- rooms/admin.py
+++ rooms/admin.py
@@ -11,7 +11,60 @@ class ItemAdmin(admin.ModelAdmin):
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
     """ Room Admin Definition"""
-    pass
+
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price")}
+        ),
+        ("Times", {"fields": ("check_in", "check_out", "instant_book")}),
+        ("Spaces", {"fields": ("amenities", "facilities", "house_rules")}),
+        ("More About the Space", {"fields": ("guests", "beds", "bedrooms", "baths")}),
+        ("Last Details", {"fields": ("hosts",)})
+    )
+
+    list_display = (
+        "name",
+        "description",
+        "country",
+        "city",
+        "price",
+        "address",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+        "count_amenities"
+    )
+
+    ordering = ("name", "price", "bedrooms")
+
+    list_filter = (
+        "instant_book",
+        "hosts__superhost",
+        "city",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
+        "country",
+    )
+
+    # if you want to using foreign key as search fields use "{foreignkey}__{field_name}"
+    search_fields = ("city", "hosts__username")
+
+    filter_horizontal = (
+        "amenities",
+        "facilities",
+        "house_rules"
+    )
+
+    # obj는 해당 row (즉 Room Row), self는 admin
+    def count_amenities(self, obj):
+        return obj.amenities.count()
 
 
 @admin.register(models.Photo)

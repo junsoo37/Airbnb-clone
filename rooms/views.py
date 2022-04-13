342ffe84e1e39f0b001f1fdd545fa9879e439fd3
junsoo37 <junsoor37@gmail.com>
2022-04-14 08:45:41 +0900

    Implement: classic pagination not using Django package

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,25 @@
+from math import ceil
 from django.shortcuts import render
 from rooms import models
 
 
 def all_rooms(request):
-    return render(request, "rooms/home.html", context={"rooms": models.Room.objects.all()})
+    page = int(request.GET.get("page", 1))
+    page_size = 10
+    offset = page_size*(page-1)
+    limit = offset+page_size
+
+    rooms = models.Room.objects.all()[offset:limit]
+    page_count = ceil(models.Room.objects.count() / page_size)
+
+    return render(
+        request,
+        "rooms/home.html",
+        context={
+            "rooms": rooms,
+            "page": page,
+            "page_count": page_count,
+            "page_range": range(1, page_count),
+          }
+    )
 

b48eac3bd90b182dd051028e923ee4819bbad01c
junsoo37 <junsoor37@gmail.com>
2022-04-17 23:46:57 +0900

    Implement: Room detail view using FBV

--- rooms/models.py
+++ rooms/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django_countries.fields import CountryField
-
+from django.urls import reverse
 from core import models as core_models
 
 
@@ -74,6 +74,9 @@ class Room(core_models.TimeStampedModel):
     def save(self, *args, **kwargs):
         self.city = str.capitalize(self.city)
         super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse("rooms:detail", kwargs={"pk": self.pk})
 
     def total_rating(self):
         all_reviews = self.reviews.all()
--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,5 @@
-from django.utils import timezone
+from django.http.response import Http404
+from django.shortcuts import render
 from django.views.generic import ListView
 from rooms import models
 
@@ -10,8 +11,10 @@ class HomeView(ListView):
     ordering = "created"
     context_object_name = "rooms"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        now = timezone.now()
-        context["now"] = now
-        return context
+
+def room_detail(request, pk):
+    try:
+        room = models.Room.objects.get(pk=pk)
+        return render(request, "rooms/room_detail.html", {"room": room})
+    except models.Room.DoesNotExist:
+        raise Http404()

d603003df1e075f99367f2e7136b393f45ff009b
junsoo37 <junsoor37@gmail.com>
2022-04-17 23:50:12 +0900

    Implement: Room detail view using CBV

--- rooms/urls.py
+++ rooms/urls.py
@@ -4,5 +4,5 @@ from rooms import views
 app_name = "rooms"
 
 urlpatterns = [
-    path("<int:pk>", views.room_detail, name="detail")
+    path("<int:pk>", views.RoomDetail.as_view(), name="detail")
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,4 @@
-from django.http.response import Http404
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from rooms import models
 
 
@@ -12,9 +10,5 @@ class HomeView(ListView):
     context_object_name = "rooms"
 
 
-def room_detail(request, pk):
-    try:
-        room = models.Room.objects.get(pk=pk)
-        return render(request, "rooms/room_detail.html", {"room": room})
-    except models.Room.DoesNotExist:
-        raise Http404()
+class RoomDetail(DetailView):
+    model = models.Room
